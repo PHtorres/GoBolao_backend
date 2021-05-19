6543d0ae20bc17a05e0edc513a0a98728f80c3cb
Paulo Henrique Torres <paulohenriquetorresdev@gmail.com>
2021-05-19 14:44:18 -0300

    Feature: bolao controller

--- src/1 - service/GoBolao.Service.API/Controllers/BolaoController.cs
+++ src/1 - service/GoBolao.Service.API/Controllers/BolaoController.cs
@@ -40,12 +40,25 @@ namespace GoBolao.Service.API.Controllers
             return Ok(ServicoBolao.CriarBolao(criarBolaoDTO, IdUsuarioAcao));
         }
 
-
         [HttpPatch]
         [Route("avatar")]
         public ActionResult<Resposta<Bolao>> Patch([FromBody] AlterarNomeImagemAvatarBolaoDTO alterarNomeImagemAvatarBolaoDTO)
         {
             return Ok(ServicoBolao.AlterarNomeImagemAvatar(alterarNomeImagemAvatarBolaoDTO, IdUsuarioAcao));
+        }
+
+        [HttpPost]
+        [Route("participar")]
+        public ActionResult<Resposta<BolaoUsuario>> PostParticipar([FromBody] ParticiparDeBolaoPublicoDTO participarDeBolaoPublicoDTO)
+        {
+            return Ok(ServicoBolao.ParticiparDeBolaoPublico(participarDeBolaoPublicoDTO, IdUsuarioAcao));
+        }
+
+        [HttpDelete]
+        [Route("sair/{idBolao}")]
+        public ActionResult<Resposta<BolaoUsuario>> DeleteSair(int idBolao)
+        {
+            return Ok(ServicoBolao.SairDeBolao(idBolao, IdUsuarioAcao));
         }
     }
 }
